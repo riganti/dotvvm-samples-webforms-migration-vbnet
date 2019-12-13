ba622423992919cc8cd3d115debea66da648784b
Tomáš Herceg <tomas.herceg@riganti.cz>
2019-12-13 19:57:38 +0100

    OWIN installed, DotVVM project created

---
+++ src/OldWebApp/OldWebApp/Startup.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Imports System.Linq.Expressions
+Imports System.Reflection
+Imports System.Web.Hosting
+Imports Microsoft.Owin
+Imports OldWebApp.DotVVM
+Imports Owin
+
+<Assembly: OwinStartup(GetType(Startup))>
+Public Class Startup
+
+    Public Sub Configuration(app As IAppBuilder)
+
+        'get DotVVM app location
+        Dim dotvvmAppLocation = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, ConfigurationManager.AppSettings("dotvvm:AppLocation"))
+
+        'register DotVVM in the pipeline
+        app.UseDotVVM(Of DotvvmStartup)(dotvvmAppLocation)
+
+    End Sub
+
+End Class
