26497c936d7516c72df8b2e42134a5c744995746
Tomáš Herceg <tomas.herceg@riganti.cz>
2019-12-13 22:28:17 +0100

    Project migrated to .NET Core

--- src/OldWebApp/OldWebApp/Global.asax.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Data.Entity
-
-Public Class Global_asax
-    Inherits HttpApplication
-
-    Sub Application_Start(sender As Object, e As EventArgs)
-        Database.SetInitializer (new AppDbInitializer())
-    End Sub
-End Class
